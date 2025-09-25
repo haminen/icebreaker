6748a14802dd6bfdb2514a4b78aca3bf458a90c7
Lassi Hämäläinen <lassi.hamalainen@elisa.fi>
2025-09-25 13:10:23 +0300

    eka ns initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//Tuodaan halutut moduulit
+
+// express-palvelin jolla mahdollistetaan webbisivut
+import express from "express";
+
+//http-kutsut ja apihommat
+import axios from "axios";
+
+//muodostetaan serveri-oli ja määritellään portti missä servu käynnistellään kun käynnistellään
+const app = express();
+const PORT = 3000;
+
+// 3. Use the public folder for static files. kuten kuvat ja tyylitiedostot
+app.use(express.static("public"));
+
+// Yksinkertainen reitti
+//app.get("/", (req, res) => {
+//  res.send("Hello, Express!");
+//});
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Programming,Dark,Pun,Spooky?blacklistFlags=racist&format=txt");
+    //console.log(response.data);
+    const activity = response.data;
+
+    // lähetetään data index.ejs:ään
+    res.render("index.ejs", { content: activity });
+  } catch (err) {
+    res.render("index.ejs", { content: "Error: " + err.message });
+  }
+});
+
+// Käynnistä serveri
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
